11b34cbc1bb46d0ba3941580e6598852fa689755
Jérôme Braeken <jeromebraeken@gmail.com>
2015-06-11 21:37:42 +0200

    Ipad fixes

---
+++ app/_site/js/jrom.js
@@ -0,0 +1,37 @@
+
+
+//making waypoints
+var waypoint = new Waypoint({
+  element: document.getElementById('welcome'),
+  handler: function(direction) {
+    // alert("Hello")
+    this.destroy()
+  }
+});
+
+var waypoint = new Waypoint({
+  element: document.getElementById('hobbies'),
+  handler: function(direction) {
+    // alert("Hobbies");
+    this.destroy()
+  }
+});
+
+var waypoint = new Waypoint({
+  element: document.getElementById('unicorn'),
+  handler: function(direction) {
+    // alert("Hello");
+    $("section .unicorn .left span").addClass("animateLeft");
+    $("section .unicorn .right span").addClass("animateRight");
+    this.destroy()
+  },
+  offset: '75%'
+});
+
+var waypoint = new Waypoint({
+  element: document.getElementById('projects'),
+  handler: function(direction) {
+    // alert("Hello");
+    this.destroy()
+  }
+});
