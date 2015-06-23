d114ab683aff6317cbaf0521537d977fc1a59afc
Jérôme Braeken <jeromebraeken@gmail.com>
2015-06-23 23:59:00 +0200

    MEGA
    3 sections (posts-home-works)
    + snap horizontaal&verticaal
    h1-hover

--- app/js/jrom.js
+++ app/js/jrom.js
@@ -21,11 +21,11 @@ var waypoint = new Waypoint({
   element: document.getElementById('unicorn'),
   handler: function(direction) {
     // alert("Hello");
-    $("section .unicorn .left span").addClass("animateLeft");
-    $("section .unicorn .right span").addClass("animateRight");
+    $(".section-wrapper .unicorn .left span").addClass("animateLeft");
+    $(".section-wrapper .unicorn .right span").addClass("animateRight");
     this.destroy()
   },
-  offset: '75%'
+  offset: '1%'
 });
 
 var waypoint = new Waypoint({
