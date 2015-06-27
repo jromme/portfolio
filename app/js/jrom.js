823126b22d5f0ec12056ac601f46887790966f94
Jérôme Braeken <jeromebraeken@gmail.com>
2015-06-27 18:02:51 +0200

    starting with css animation when section active

--- app/js/jrom.js
+++ app/js/jrom.js
@@ -16,22 +16,3 @@ var waypoint = new Waypoint({
     this.destroy()
   }
 });
-
-var waypoint = new Waypoint({
-  element: document.getElementById('unicorn'),
-  handler: function(direction) {
-    // alert("Hello");
-    $(".section-wrapper .unicorn .left span").addClass("animateLeft");
-    $(".section-wrapper .unicorn .right span").addClass("animateRight");
-    this.destroy()
-  },
-  offset: '1%'
-});
-
-var waypoint = new Waypoint({
-  element: document.getElementById('projects'),
-  handler: function(direction) {
-    // alert("Hello");
-    this.destroy()
-  }
-});
