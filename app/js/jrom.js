a3cc7a3ef3a076c1364657840a4f29924c100b7f
Jérôme Braeken <jeromebraeken@gmail.com>
2015-06-29 15:04:08 +0200

    clear code (menu)

--- app/js/jrom.js
+++ app/js/jrom.js
@@ -1,73 +1,78 @@
 // 'use strict';
 
-//making waypoints
-var waypoint = new Waypoint({
-  element: document.getElementById('welcome'),
-  handler: function(direction) {
-    // alert("Hello")
-    this.destroy()
-  }
-});
+$(document).ready(function() {
 
-var waypoint = new Waypoint({
-  element: document.getElementById('hobbies'),
-  handler: function(direction) {
-    // alert("Hobbies");
-    this.destroy()
-  }
-});
-
-  //typed
-$('.intro .left .wrap:nth-child(1) h1').on('click', function(){
-  $(".intro .left .wrap:nth-child(1) p").addClass('cursor'),
-  $(".intro .left .wrap:nth-child(1) p").typed({
-    strings: ["is Jerome Braeken, born in 1991. What will happen next when I add a line."],
-    typeSpeed: 0,
-    showCursor: false
+  //panelsnap
+  $('#posts .panels').panelSnap({
+    $menu: $('#posts #menu .menu')
   });
-});
 
-$('.intro .left .wrap:nth-child(2) h1').on('click',function(){
-  $(".intro .left .wrap:nth-child(2) p").addClass('cursor'),
-  $(".intro .left .wrap:nth-child(2) p").typed({
-    strings: ["raised in Maastricht. A city close to both Belgium and Germany."],
-    typeSpeed: 0,
-    showCursor: false
+  $('#home .panels').panelSnap();
+
+  $('#works .panels').panelSnap({
+    $menu: $('#works #menu .menu')
   });
-});
 
-$('.intro .right .wrap:nth-child(1) h1').on('click', function(){
-  $(".intro .right .wrap:nth-child(1) p").addClass('cursor'),
-  $(".intro .right .wrap:nth-child(1) p").typed({
-    strings: ["what I do? Creating things is like a drug to me. It's even worse when people like it."],
-    typeSpeed: 0,
-    showCursor: true
+  //snapp
+  $(document).scrollsnap({
+      direction: 'x',
+      snaps: '.snap',
+      proximity: 1000,
+      latency: 50
   });
-});
 
-$('.intro .right .wrap:nth-child(2) h1').on('click', function(){
-  $(".intro .right .wrap:nth-child(2) p").addClass('cursor'),
-  $(".intro .right .wrap:nth-child(2) p").typed({
-    strings: ["am studying CMD at Zuyd Hogeschool in Maastricht. Looking for a half year internship, starting from September."],
-    typeSpeed: 0,
-    showCursor: true
+    //typed
+  $('.intro .left .wrap:nth-child(1) h1').on('click', function(){
+    $(".intro .left .wrap:nth-child(1) p").addClass('cursor'),
+    $(".intro .left .wrap:nth-child(1) p").typed({
+      strings: ["is Jerome Braeken, born in 1991. What will happen next when I add a line."],
+      typeSpeed: 0,
+      showCursor: false
+    });
   });
-});
 
-$('.left .wrap:nth-child(1) h1').click(function(){
-  $('.left .wrap:nth-child(1) h1 span.typed-cursor').css('display', 'none')
-});
+  $('.intro .left .wrap:nth-child(2) h1').on('click',function(){
+    $(".intro .left .wrap:nth-child(2) p").addClass('cursor'),
+    $(".intro .left .wrap:nth-child(2) p").typed({
+      strings: ["raised in Maastricht. A city close to both Belgium and Germany."],
+      typeSpeed: 0,
+      showCursor: false
+    });
+  });
 
-$('.left .wrap:nth-child(2) h1').click(function(){
-  $('.left .wrap:nth-child(2) h1 span.typed-cursor').css('display', 'none')
-});
+  $('.intro .right .wrap:nth-child(1) h1').on('click', function(){
+    $(".intro .right .wrap:nth-child(1) p").addClass('cursor'),
+    $(".intro .right .wrap:nth-child(1) p").typed({
+      strings: ["what I do? Creating things is like a drug to me. It's even worse when people like it."],
+      typeSpeed: 0,
+      showCursor: true
+    });
+  });
 
-$('.right .wrap:nth-child(1) h1').click(function(){
-  $('.right .wrap:nth-child(1) h1 span.typed-cursor').css('display', 'none')
-});
+  $('.intro .right .wrap:nth-child(2) h1').on('click', function(){
+    $(".intro .right .wrap:nth-child(2) p").addClass('cursor'),
+    $(".intro .right .wrap:nth-child(2) p").typed({
+      strings: ["am studying CMD at Zuyd Hogeschool in Maastricht. Looking for a half year internship, starting from September."],
+      typeSpeed: 0,
+      showCursor: true
+    });
+  });
 
-$('.right .wrap:nth-child(2) h1').click(function(){
-  $('.right .wrap:nth-child(2) h1 span.typed-cursor').css('display', 'none')
+  $('.left .wrap:nth-child(1) h1').click(function(){
+    $('.left .wrap:nth-child(1) h1 span.typed-cursor').css('display', 'none')
+  });
+
+  $('.left .wrap:nth-child(2) h1').click(function(){
+    $('.left .wrap:nth-child(2) h1 span.typed-cursor').css('display', 'none')
+  });
+
+  $('.right .wrap:nth-child(1) h1').click(function(){
+    $('.right .wrap:nth-child(1) h1 span.typed-cursor').css('display', 'none')
+  });
+
+  $('.right .wrap:nth-child(2) h1').click(function(){
+    $('.right .wrap:nth-child(2) h1 span.typed-cursor').css('display', 'none')
+  });
 });
 
 
