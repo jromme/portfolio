49116ed7789de09c54e11ca05c11b501f739b359
Jérôme Braeken <jeromebraeken@gmail.com>
2015-07-08 20:39:18 +0200

    Winter is coming...

--- app/js/jrom.js
+++ app/js/jrom.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
   $('.intro .right .wrap:nth-child(2) h1').on('click', function(){
     $(".intro .right .wrap:nth-child(2) p").addClass('cursor'),
     $(".intro .right .wrap:nth-child(2) p").typed({
-      strings: ["am studying Communication and Design at Zuyd Hogeschool in Maastricht. Looking for a half year internship, starting from September. So hurry, winter is coming..."],
+      strings: ["am studying Communication and Design at Zuyd Hogeschool in Maastricht. At the moment I am looking for a half year internship, starting from September. So hurry, winter is coming..."],
       typeSpeed: 0,
       showCursor: true
     });
