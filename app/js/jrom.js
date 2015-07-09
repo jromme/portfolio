fdd9bd1b9e7501c6bb9145d9639381d0e2b5529e
Jérôme Braeken <jeromebraeken@gmail.com>
2015-07-09 15:46:57 +0200

    smooth horizontal scroll

--- app/js/jrom.js
+++ app/js/jrom.js
@@ -2,7 +2,23 @@
 
 $(document).ready(function() {
 
-   $(window).onload = location.href="#home";
+  $(window).onload = location.href="#home";
+
+  //smooth h-scroll
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollLeft: target.offset().left
+          }, 500);
+          return false;
+        }
+      }
+    });
+  });
 
   //panelsnap
   $('#posts .panels').panelSnap({
